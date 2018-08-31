63849dc9de2bfd75ab1e3b91713866c4337641ea
Coolzapper Modding <28998760+CZModding@users.noreply.github.com>
2018-08-31 12:50:38 +0400

    Base files
    Base files have been added :-
    
    Evilcraft.class
    Reference.class
    mcmod.info

---
+++ src/main/java/com/czmodding/evilcraft/Reference.java
@@ -0,0 +1,9 @@
+package com.czmodding.evilcraft;
+
+public class Reference {
+	
+		public static final String  MODID = "ec";
+		public static final String MODNAME = "Evil Craft";
+		public static final String VERSION = "a1.0";
+		public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
+}
