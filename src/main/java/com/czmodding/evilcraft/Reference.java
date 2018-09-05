8f96f5baed8904039d7dac7b83bceb219021fd41
Coolzapper Modding <28998760+CZModding@users.noreply.github.com>
2018-09-05 18:03:10 +0400

    Tweaked a few things...

--- src/main/java/com/czmodding/evilcraft/Reference.java
+++ src/main/java/com/czmodding/evilcraft/Reference.java
@@ -2,7 +2,7 @@ package com.czmodding.evilcraft;
 
 public class Reference {
 	
-		public static final String  MODID = "ec";
+		public static final String MODID = "evilcraft";
 		public static final String MODNAME = "Evil Craft";
 		public static final String VERSION = "a1.0";
 		public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
